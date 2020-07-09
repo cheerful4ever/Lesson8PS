be46cda1dfcc68c817b235ef4532314bf3bad506
18016375 <cecilialui16@yahoo.com.sg>
2020-07-09 21:48:41 +0800

    activities 1-100000000

--- app/src/main/java/com/example/lesson8ps/MainActivity.java
+++ app/src/main/java/com/example/lesson8ps/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.lesson8ps;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -11,6 +12,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -39,17 +41,6 @@ public class MainActivity extends AppCompatActivity {
             public void onMapReady(GoogleMap googleMap) {
                 map = googleMap;
 
-                int permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this,
-                        android.Manifest.permission.ACCESS_FINE_LOCATION);
-
-                if (permissionCheck == PermissionChecker.PERMISSION_GRANTED) {
-                    map.setMyLocationEnabled(true);
-                } else {
-                    Log.e("GMap - Permission", "GPS access has not been granted");
-                    ActivityCompat.requestPermissions(MainActivity.this,
-                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
-                }
-
                 LatLng poi_north = new LatLng(1.461708, 103.813500);
                 Marker north = map.addMarker(new
                         MarkerOptions()
@@ -57,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
                         .title("North - HQ")
                         .snippet("Block 333, Admiralty Ave 3, 765654 Operating hours: 10am-5pm\n" +
                                 "Tel:65433456\n")
-                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+                        .icon(BitmapDescriptorFactory.fromResource(android.R.drawable.star_big_on)));
 
                 LatLng poi_central = new LatLng(1.300542, 103.841226);
                 Marker central = map.addMarker(new
@@ -79,7 +70,27 @@ public class MainActivity extends AppCompatActivity {
                                 "Tel:66776677\"\n")
                         .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
 
-
+                map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+                    @Override
+                    public boolean onMarkerClick(Marker marker) {
+                        String name = marker.getTitle();
+                        if (name.equals("North - HQ")) {
+                            Toast.makeText(MainActivity.this,
+                                    "North - HQ", Toast.LENGTH_SHORT).show();
+                            return true;
+                        } else if (name.equals("Central")) {
+                            Toast.makeText(MainActivity.this,
+                                    "Central", Toast.LENGTH_SHORT).show();
+                            return true;
+                        } else if (name.equals("East")) {
+                            Toast.makeText(MainActivity.this,
+                                    "East", Toast.LENGTH_SHORT).show();
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }
+                });
             }
         });
 
@@ -117,5 +128,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        int permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if (permissionCheck == PermissionChecker.PERMISSION_GRANTED) {
+            map.setMyLocationEnabled(true);
+        } else {
+            Log.e("GMap - Permission", "GPS access has not been granted");
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+        }
     }
 }
